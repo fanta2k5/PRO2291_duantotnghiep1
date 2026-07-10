e5d35e67311b8ef0b69c7fcaa8825b46751bfcb9
baolesg <baolesg@gmail.com>
2026-07-10 20:39:56 +0700

    abc

--- src/load_sql.py
+++ src/load_sql.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine, text
 # KẾT NỐI SQL SERVER
 # =========================
 
-SERVER = r"LAPTOP-87EUJRCL\MSSQLSERVER1"
+SERVER = r"BAOLESG\MSSQLSERVER1"
 DATABASE = "SalesAnalyticsDW"
 
 connection_string = (
